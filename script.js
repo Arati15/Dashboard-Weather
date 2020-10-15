d8ddba9839c890b121cba832a2898fdc41b9d06a
Arati Bhandwalkar <arativbhandwalkar@gmail.com>
2020-10-15 12:21:51 -0700

    check current weather function works

--- script.js
+++ script.js
@@ -13,23 +13,23 @@ if (localStorage.getItem("localWeatherSearches")) {
 };
 
 //function returns current weather
-function currentWeather(cityName) {
-    //display current weather
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+// function currentWeather(cityName) {
+//     //display current weather
+//     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
 
-    $.get(queryURL).then(function (response) {
-        let currTime = new Date(response.dt * 1000);
-        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
-        //styling
-        currWeatherDiv.html(`
-        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth() + 1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
-        <p>Temperature: ${response.main.temp} &#176;C</p>
-        <p>Humidity: ${response.main.humidity}%</p>
-        <p>Wind Speed: ${response.wind.speed} m/s</p>
-        `, returnUVIndex(response.coord))
-        createHistoryButton(response.name);
-    })
-};
+//     $.get(queryURL).then(function (response) {
+//         let currTime = new Date(response.dt * 1000);
+//         let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+//         //styling
+//         currWeatherDiv.html(`
+//         <h2>${response.name}, ${response.sys.country} (${currTime.getMonth() + 1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+//         <p>Temperature: ${response.main.temp} &#176;C</p>
+//         <p>Humidity: ${response.main.humidity}%</p>
+//         <p>Wind Speed: ${response.wind.speed} m/s</p>
+//         `, returnUVIndex(response.coord))
+//         createHistoryButton(response.name);
+//     })
+// };
 
 //function returns weather forecast 
 function weatherForecast(cityName) {
